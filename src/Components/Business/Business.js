c7beb53804ae8a60b28617ed613e0197679d8503
DestruS96 <mat820@hotmail.co.uk>
2020-12-25 12:19:29 +0000

    Part 2

--- src/Components/Business/Business.js
+++ src/Components/Business/Business.js
@@ -1,20 +1,9 @@
 import React, { Component } from 'react'
 import './Business.css';
 
-const business = {
-    imageSrc: 'https://content.codecademy.com/programs/react/ravenous/pizza.jpg',
-    name: 'MarginOtto Pizzeria',
-    address: '1010 Paddington Way',
-    city: 'Flavortown',
-    state: 'NY',
-    zipCode: '10101',
-    category: 'Italian',
-    rating: 4.5,
-    reviewCount: 90
-}
-
 export class Business extends Component {
     render() {
+        const { business } = this.props;
         return (
             <div className="Business">
                 <div className="image-container">
@@ -25,12 +14,12 @@ export class Business extends Component {
                     <div className="Business-address">
                         <p>{business.address}</p>
                         <p>{business.city}</p>
-                        <p>{business.state + '' + business.zipCode}</p>
+                        <p>{business.state + ' ' + business.zipCode}</p>
                     </div>
                     <div className="Business-reviews">
                         <h3>{business.category}</h3>
-                        <h3 className="rating">{business.rating}</h3>
-                        <p>{business.reviewCount}</p>
+                        <h3 className="rating">{business.rating} stars</h3>
+                        <p>{business.reviewCount} reviews</p>
                     </div>
                 </div>
             </div>
